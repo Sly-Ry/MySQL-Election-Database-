8de7f3797854558f658beaa692b873b9bb64dab6
ryan <ryan.williams93@aol.com>
2022-01-25 16:46:42 -0600

    built the database calls

--- server.js
+++ server.js
@@ -21,7 +21,15 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
-// Default response for any other request (Not Found)
+// db.query() - runs the SQL query and executes the callback with all the resulting rows that match the query.Once this method executes the SQL command, the callback function captures the responses from the query in two variables.
+// err - the error response.
+// rows - the database query response.
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
+// Default response for any other request (Not Found). [catchall route]
 app.use((req, res) => {
     res.status(404).end();
 });
