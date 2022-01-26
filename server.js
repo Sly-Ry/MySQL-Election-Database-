9b2f444546e106a0f39cf9de338a836597f57a66
ryan <ryan.williams93@aol.com>
2022-01-26 16:16:35 -0600

    created a GET route for front end

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
+const { addHook } = require('pirates');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -21,22 +22,46 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
-// db.query() - runs the SQL query and executes the callback with all the resulting rows that match the query.Once this method executes the SQL command, the callback function captures the responses from the query in two variables.
-// err - the error response.
-// rows - the database query response.
-
-// db.query(`SELECT * FROM candidates`, (err, rows) => {
-//     console.log(rows);
-// });
+// GET all candidates.
+// The api in the URL signifies that this is an API endpoint.
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    // db.query() - runs the SQL query and executes the callback with all the resulting rows that match the query.Once this method executes the SQL command, the callback function captures the responses from the query in two variables.
+    // err - the error response.
+    // rows - the database query response.
+    db.query(sql, (err, rows) => {
+        if (err) {
+            // Instead of logging the error, we'll send a status code of 500 and place the error message within a JSON object. 
+            res.status(500).json({ error: err.messgae });
+            return;
+        }
+        // If there was no error, then err is null and the response is sent back using the following statement:
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
 
 // GET a single candidate
+app.get ('/api/candidates/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`
+    const params = [req.params.id];
 
-// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-//     if(err) {
-//         console.log(err);
-//     }
-//     console.log(row);
-// });
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            // Instead of logging the error, we'll send a status code of 500 and place the error message within a JSON object. 
+            res.status(400).json({ error: err.messgae });
+            return;
+        }
+        // If there was no error, then err is null and the response is sent back using the following statement:
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
 
 // Delete a candidate
 // ? - placeholder, making this a prepared statement. 
@@ -54,19 +79,20 @@ const db = mysql.createConnection(
 // The SQL command and the SQL parameters were assigned to the sql and params variables respectively to improve the legibility for the call function to the database.
 // INSERT INTO - adds the values that are assigned to param.
 // VALUES (?,?,?,?) - because the candidates table includes four columns [id, first_name, last_name, industry_connected] we need four placeholders for those four values. 
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-            VALUES (?,?,?,?)`
-;
 
-// The four placeholders must match the four values in params, so we must use an array.
-const params = [1, 'Ronald', 'Firbank', 1];
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//             VALUES (?,?,?,?)`
+// ;
 
-db.query(sql, params, (err, results) =>{
-    if(err) {
-        console.log(err);
-    }
-    console.log(results)
-})
+// // The four placeholders must match the four values in params, so we must use an array.
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, results) =>{
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(results)
+// });
 
 // Default response for any other request (Not Found). [catchall route]
 app.use((req, res) => {
