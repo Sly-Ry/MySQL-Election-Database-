22ad6ddcb7b5713dd1b052db349bfda74775ab2a
ryan <ryan.williams93@aol.com>
2022-01-28 14:57:39 -0600

    made GET route for single voter

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const db = require('../../db/connection');
 
 router.get('/voters', (req, res) => {
-    const sql = `SELECT * FROM voters`;
+    const sql = `SELECT * FROM voters ORDER BY last_name`;
 
     db.query(sql, (err, rows) => {
         if (err) {
@@ -13,6 +13,22 @@ router.get('/voters', (req, res) => {
         res.json({
             message: 'success',
             data: rows
+        });
+    });
+});
+
+router.get('/voter/:id', (req, res) => {
+    const sql = `SELECT * FROM voters WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'Success',
+            data: row
         });
     });
 });
