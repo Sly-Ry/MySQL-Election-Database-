a43fecde6b91f87b80189bdecb1306ee76ed3581
ryan <ryan.williams93@aol.com>
2022-12-07 17:29:53 -0600

    testing MYSQL

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection(
         // Your MySQL username,
         user: 'root',
         // Your MySQL password
-        password: 'eNderRoot1993',
+        password: '',
         database: 'election'
     },
     console.log('Connected to the election database.')
